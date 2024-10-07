d00e6b82a692a44a6e8781804b093e9dca51fc58
lstraeb1 <liamstraebler@gmail.com>
2024-10-07 17:55:29 -0400

    Blog

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -16,25 +16,25 @@ function disableDarkMode() {
 }
 
 // Determines a new user's dark mode preferences
-// function detectColorScheme() {
-// 	// Default to the light theme
-// 	let theme = 'light';
+function detectColorScheme() {
+	// Default to the light theme
+	let theme = 'light';
 
-// 	// Check localStorage for a saved 'theme' variable. If it's there, the user has visited before, so apply the necessary theme choices
-// 	if (localStorage.getItem('theme')) {
-// 		theme = localStorage.getItem('theme');
-// 	}
-// 	// If it's not there, check to see if the user has applied dark mode preferences themselves in the browser
-// 	else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-// 		theme = 'dark';
-// 	}
+	// Check localStorage for a saved 'theme' variable. If it's there, the user has visited before, so apply the necessary theme choices
+	if (localStorage.getItem('theme')) {
+		theme = localStorage.getItem('theme');
+	}
+	// If it's not there, check to see if the user has applied dark mode preferences themselves in the browser
+	else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		theme = 'dark';
+	}
 
-// 	// Apply the theme accordingly
-// 	theme === 'dark' ? enableDarkMode() : disableDarkMode();
-// }
+	// Apply the theme accordingly
+	theme === 'dark' ? enableDarkMode() : disableDarkMode();
+}
 
-// // Run on page load
-// detectColorScheme();
+// Run on page load
+detectColorScheme();
 
 // Add event listener to the dark mode button toggle
 document.getElementById('dark-mode-toggle').addEventListener('change', () => {
