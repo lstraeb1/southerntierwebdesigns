0dc3d1f9610239787aaa97d40242522697cfb82a
Liam Straebler <liamstraebler@gmail.com>
2026-01-19 13:01:09 -0500

    OG image update

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,19 +1,19 @@
 module.exports = {
-    "name": "Leadwell Growth",
-    "email": "liam@southerntierwebdesigns.com",
-    "phoneForTel": "845-754-6086",
-    "phoneFormatted": "(845) 754-6086",
-    "address": {
-        "lineOne": "238 West Main St.",
-        "city": "Port Jervis",
-        "state": "NY",
-        "zip": "12771",
-        "mapLink": ""
+    name: "Leadwell Growth",
+    email: "liam@southerntierwebdesigns.com",
+    phoneForTel: "845-754-6086",
+    phoneFormatted: "(845) 754-6086",
+    address: {
+        lineOne: "238 West Main St.",
+        city: "Port Jervis",
+        state: "NY",
+        zip: "12771",
+        mapLink: ""
     },
-    "socials": {
-        "facebook": "-",
-        "instagram": "-",
+    socials: {
+        facebook: "-",
+        instagram: "-",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://leadwellgrowth.com"
+    domain: "https://leadwellgrowth.com"
 };
